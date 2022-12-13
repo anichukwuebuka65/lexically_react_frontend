a72cc78aad9c51393728fd2575b02328f736bb7c
anichukwuebuka <anichukwuebuka65@gmail.com>
2022-12-13 22:57:42 +0000

    change again

--- src/App.js
+++ src/App.js
@@ -1,89 +1,123 @@
-import React, { Component } from 'react'
-import PhotoDetails from "./components/photoDetails"
-import Collections from "./components/collections/collections"                                                                                                          
-import CollectionsDetails from "./components/collections/collectionsDetails"                                                                                                          
-import {Routes, Route} from "react-router-dom"
-import Layout from './components/layout'
-import Home from './components/home/home'
-import SearchResults from './components/searchResults'
-import WithNavigate from './components/withNavigate'
-import IntroSection from "./components/home/introSection"
-import SearchInput from "./components/home/searchInput"
-import MyCollections from './components/collections/myCollections'
-import Photos from './components/photos'
-import NavBar from './components/home/navBar'
-import CollectionHeader from './components/collections/collectionHeader'
-import SignIn from './components/signIn'
+import React, { Component } from "react";
+import PhotoDetails from "./components/photoDetails";
+import Collections from "./components/collections/collections";
+import CollectionsDetails from "./components/collections/collectionsDetails";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./components/home/home";
+import SearchResults from "./components/searchResults";
+import WithNavigate from "./components/withNavigate";
+import IntroSection from "./components/home/introSection";
+import SearchInput from "./components/home/searchInput";
+import MyCollections from "./components/collections/myCollections";
+import Photos from "./components/photos";
+import NavBar from "./components/home/navBar";
+import CollectionHeader from "./components/collections/collectionHeader";
+import SignIn from "./components/signIn";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       photos: [],
-      showMenuButton: true,
+      showMenuButton: false,
       firstRender: true,
-      isUploadModalOpen: false
-
-    }
-    this.username = React.createRef()
-    this.layoutRef = React.createRef()
+      isUploadModalOpen: false,
+    };
+    this.username = React.createRef();
+    this.layoutRef = React.createRef();
   }
 
   displayButton = () => {
-    this.layoutRef.current.buttonRef.current.style.display = "block"
-  }
+    this.layoutRef.current.buttonRef.current.style.display = "block";
+  };
 
   handleFetch = (items) => {
-    this.setState((state) =>({ photos:[...state.photos, ...items]}))
-  }
+    this.setState((state) => ({ photos: [...state.photos, ...items] }));
+  };
 
   handleSearch = (items) => {
-    this.setState({photos: items})
-  }
+    this.setState({ photos: items });
+  };
 
   toggleMenuButton = () => {
     this.setState((state) => ({
       ...state,
       showMenuButton: !state.showMenuButton,
-      firstRender: false
-    }))
-  }
+      firstRender: false,
+    }));
+  };
 
   toggleUploadModal = () => {
-    this.setState((state) => ({...state, isUploadModalOpen: !state.isUploadModalOpen}))
-  }
+    this.setState((state) => ({
+      ...state,
+      isUploadModalOpen: !state.isUploadModalOpen,
+    }));
+  };
 
   render() {
-    const { photos, firstRender, showMenuButton , isUploadModalOpen} = this.state
+    const { photos, firstRender, showMenuButton, isUploadModalOpen } =
+      this.state;
 
     return (
       <Routes>
-        <Route path="/" element={
-          <Layout 
-            isUploadModalOpen={isUploadModalOpen} 
-            ref={this.layoutRef}
-            signIn={<SignIn token={this.props.token}/>} 
-            render={data => <NavBar firstRender={firstRender} toggleMenuButton={this.toggleMenuButton} showMenuButton={showMenuButton} toggleUploadModal={data}/>}
+        <Route
+          path="/"
+          element={
+            <Layout
+              isUploadModalOpen={isUploadModalOpen}
+              ref={this.layoutRef}
+              signIn={<SignIn token={this.props.token} />}
+              render={(data) => (
+                <NavBar
+                  firstRender={firstRender}
+                  toggleMenuButton={this.toggleMenuButton}
+                  showMenuButton={showMenuButton}
+                  toggleUploadModal={data}
+                />
+              )}
             >
-            <IntroSection >
-              <SearchInput handleSearch={this.handleSearch}/>
-            </IntroSection>
-          </Layout>}>
-          <Route index element={
-                <Home photos={photos} displayButton={this.displayButton} handleFetch={this.handleFetch} />
-              } />
-          <Route path="search" element = { <SearchResults photos={photos} />} />
-          <Route path="collections" element={<Collections render={ data => (<Photos photos={data} link="collections"/>)}>
-            <CollectionHeader toggleUploadModal={this.toggleUploadModal} />
-          </Collections>} />
-          <Route path="my-collections" element={<MyCollections/>} />
-          <Route path="my-collections/:id" element={<CollectionsDetails displayButton={this.displayButton}/>}/>
-          <Route path="photo-detail/:id" element = {<PhotoDetails />} />
-          <Route path="collections/:id" element = {<CollectionsDetails displayButton={this.displayButton}/>} />
+              <IntroSection>
+                <SearchInput handleSearch={this.handleSearch} />
+              </IntroSection>
+            </Layout>
+          }
+        >
+          <Route
+            index
+            element={
+              <Home
+                photos={photos}
+                displayButton={this.displayButton}
+                handleFetch={this.handleFetch}
+              />
+            }
+          />
+          <Route path="search" element={<SearchResults photos={photos} />} />
+          <Route
+            path="collections"
+            element={
+              <Collections
+                render={(data) => <Photos photos={data} link="collections" />}
+              >
+                <CollectionHeader toggleUploadModal={this.toggleUploadModal} />
+              </Collections>
+            }
+          />
+          <Route path="my-collections" element={<MyCollections />} />
+          <Route
+            path="my-collections/:id"
+            element={<CollectionsDetails displayButton={this.displayButton} />}
+          />
+          <Route path="photo-detail/:id" element={<PhotoDetails />} />
+          <Route
+            path="collections/:id"
+            element={<CollectionsDetails displayButton={this.displayButton} />}
+          />
         </Route>
       </Routes>
-    )
+    );
   }
 }
 
-export default WithNavigate(App)
+export default WithNavigate(App);
--- src/components/home/navBar.js
+++ src/components/home/navBar.js
@@ -1,47 +1,92 @@
-import React, { Component } from 'react'
-import { AiOutlineMenu } from "react-icons/ai"
-import { IoClose } from "react-icons/io5"
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { IoClose } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 class NavBar extends Component {
-
-  constructor(){
-    super()
-    this.state = {
-      
-    }
-    this.username = localStorage.getItem("lexically__name")
+  constructor() {
+    super();
+    this.state = {};
+    this.username = localStorage.getItem("lexically__name");
   }
 
   render() {
-    const {showMenuButton, firstRender, toggleMenuButton, toggleUploadModal} = this.props
+    const { showMenuButton, firstRender, toggleMenuButton, toggleUploadModal } =
+      this.props;
 
     return (
-      <header className='fixed right-0 left-0 z-40'>
+      <header className="fixed right-0 left-0 z-40">
         <div className="z-20 relative flex justify-between items-center h-14 bg-slate-100 ">
-          <Link to="/" className='md:text-4xl md:px-6  italic font-black rounded-sm ml-2 font-mono'>
-          <span>Lexical</span>
+          <Link
+            to="/"
+            className="md:text-4xl md:px-6  italic font-black rounded-sm ml-2 font-mono"
+          >
+            <span>Lexical</span>
           </Link>
-          <div className='sm:flex hidden font-mono justify-center items-center space-x-6 mr-8 text-lg'>
-            <Link to="/" className='hover:cursor-pointer hover:underline'>Home</Link>
-            <Link to="/collections" className=' hover:cursor-pointer hover:underline'>collections</Link>
-            {!this.username && <button onClick={toggleUploadModal} className='flex items-center space-x-2 font-bold font-mono rounded-full py-px px-3 tracking-wide bg-emerald-900 text-white '>
-              <b>Sign In</b>
-            </button>}
+          <div className="sm:flex hidden font-mono justify-center items-center space-x-6 mr-8 text-lg">
+            <Link to="/" className="hover:cursor-pointer hover:underline">
+              Home
+            </Link>
+            <Link
+              to="/collections"
+              className=" hover:cursor-pointer hover:underline"
+            >
+              collections
+            </Link>
+            {!this.username && (
+              <button
+                onClick={toggleUploadModal}
+                className="flex items-center space-x-2 font-bold font-mono rounded-full py-px px-3 tracking-wide bg-emerald-900 text-white "
+              >
+                <b>Sign In</b>
+              </button>
+            )}
           </div>
-          <button onClick={toggleMenuButton} className={`${showMenuButton && "hidden"} sm:hidden mr-8 text-2xl hover:cursor-pointer`}><AiOutlineMenu/></button>
-          <button onClick={toggleMenuButton} className={`${!showMenuButton && "hidden"} sm:hidden mr-8 text-2xl hover:cursor-pointer`}><IoClose/></button>
+          <button
+            onClick={toggleMenuButton}
+            className={`${
+              showMenuButton && "hidden"
+            } sm:hidden mr-8 text-2xl hover:cursor-pointer`}
+          >
+            <AiOutlineMenu />
+          </button>
+          <button
+            onClick={toggleMenuButton}
+            className={`${
+              !showMenuButton && "hidden"
+            } sm:hidden mr-8 text-2xl hover:cursor-pointer`}
+          >
+            <IoClose />
+          </button>
         </div>
-        <div className={`absolute ${!showMenuButton && !firstRender ? "animate-hideslow" : showMenuButton && "animate-dropslow "}
+        <div
+          className={`absolute ${
+            !showMenuButton && !firstRender
+              ? "animate-hideslow"
+              : showMenuButton && "animate-dropslow "
+          }
          z-0 -top-14 sm:hidden font-semibold  border-b-2 border-slate-500 flex flex-col justify-content-start
-         space-y-2 text-xl text-white py-1.5 px-3 bg-cyan-900 w-full`}>
-          <Link to="/" onClick={toggleMenuButton} >Home</Link>
-          <Link to="/collections" onClick={toggleMenuButton} >Collections</Link>
-         {!this.username && <div role="button" className='justify-self-start' onClick={this.props.toggleUploadModal}>Sign In</div>}
+         space-y-2 text-xl text-white py-1.5 px-3 bg-cyan-900 w-full`}
+        >
+          <Link to="/" onClick={toggleMenuButton}>
+            Home
+          </Link>
+          <Link to="/collections" onClick={toggleMenuButton}>
+            Collections
+          </Link>
+          {!this.username && (
+            <div
+              role="button"
+              className="justify-self-start"
+              onClick={this.props.toggleUploadModal}
+            >
+              Sign In
+            </div>
+          )}
         </div>
       </header>
-    )
+    );
   }
 }
 
-export default NavBar
+export default NavBar;
